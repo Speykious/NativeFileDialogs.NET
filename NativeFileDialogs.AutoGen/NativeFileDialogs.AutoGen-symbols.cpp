5b6a39a6aa4c56d488d4b3d266abe093dd1fcc4f
Speykious <speykious@gmail.com>
2022-01-30 19:50:09 +0100

    Generate bindings

---
+++ NativeFileDialogs.AutoGen/NativeFileDialogs.AutoGen-symbols.cpp
@@ -0,0 +1,5 @@
+#include <nfd.h>
+#include <new>
+
+nfdpathsetenum_t& (nfdpathsetenum_t::*_0)(nfdpathsetenum_t&&) = &nfdpathsetenum_t::operator=;
+nfdnfilteritem_t& (nfdnfilteritem_t::*_1)(nfdnfilteritem_t&&) = &nfdnfilteritem_t::operator=;
