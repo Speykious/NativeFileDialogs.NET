82ed3949031c2c5fb938cfb99dc41050973e7e3d
Speykious <speykious@gmail.com>
2022-01-31 17:28:20 +0100

    Write first example

---
+++ NativeFileDialogs.Net.Example.OpenDialog/Program.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Speykious
+// This file is part of NativeFileDialogs.NET.
+// NativeFileDialogs.NET is licensed under the Zlib License. See LICENSE for details.
+
+using NativeFileDialogs.AutoGen;
+
+unsafe
+{
+    nfd.NFD_Init();
+
+    NfdnfilteritemT[] filterItem = new[]
+    {
+        new NfdnfilteritemT
+        {
+            Name = "Source code",
+            Spec = "c,cpp,cc",
+        },
+        new NfdnfilteritemT
+        {
+            Name = "Headers",
+            Spec = "h,hpp",
+        },
+    };
+
+    sbyte* outPathPtr;
+    NfdresultT result = nfd.NFD_OpenDialogN(&outPathPtr, filterItem, (uint)filterItem.Length, null);
+    switch (result)
+    {
+        case NfdresultT.NFD_OKAY:
+            Console.WriteLine("Success!");
+            Console.WriteLine(new string(outPathPtr));
+            break;
+        case NfdresultT.NFD_CANCEL:
+            Console.WriteLine("User pressed Cancel.");
+            break;
+        default:
+            Console.WriteLine($"Error: {nfd.NFD_GetError()}");
+            break;
+    }
+
+    nfd.NFD_Quit();
+}
