597cedbb26b4054e864424d115805deded07755e
Speykious <speykious@gmail.com>
2022-03-27 14:43:09 +0200

    Complete PickFolder example

--- Examples/AutoGen/NativeFileDialogs.Net.Example.PickFolder/Program.cs
+++ Examples/AutoGen/NativeFileDialogs.Net.Example.PickFolder/Program.cs
@@ -2,13 +2,28 @@
 // This file is part of NativeFileDialogs.NET.
 // NativeFileDialogs.NET is licensed under the Zlib License. See LICENSE for details.
 
+using System;
 using NativeFileDialogs.AutoGen;
 
 unsafe
 {
     nfd.NFD_Init();
 
-
+    sbyte* outPathPtr;
+    NfdresultT result = nfd.NFD_PickFolderN(&outPathPtr, null);
+    switch (result)
+    {
+        case NfdresultT.NFD_OKAY:
+            Console.WriteLine("Success!");
+            Console.WriteLine(new string(outPathPtr));
+            break;
+        case NfdresultT.NFD_CANCEL:
+            Console.WriteLine("User pressed Cancel.");
+            break;
+        default:
+            Console.WriteLine($"Error: {nfd.NFD_GetError()}");
+            break;
+    }
 
     nfd.NFD_Quit();
 }
