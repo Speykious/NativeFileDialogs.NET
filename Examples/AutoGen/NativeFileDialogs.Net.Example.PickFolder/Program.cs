8ba1fc12ba12218009e976295320bc9142491d83
Speykious <speykious@gmail.com>
2022-03-27 14:31:43 +0200

    Create `PickFolder` example

---
+++ Examples/AutoGen/NativeFileDialogs.Net.Example.PickFolder/Program.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Speykious
+// This file is part of NativeFileDialogs.NET.
+// NativeFileDialogs.NET is licensed under the Zlib License. See LICENSE for details.
+
+using NativeFileDialogs.AutoGen;
+
+unsafe
+{
+    nfd.NFD_Init();
+
+
+
+    nfd.NFD_Quit();
+}
