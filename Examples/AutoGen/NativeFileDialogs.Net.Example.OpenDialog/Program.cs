3a8b4a17496a2612acec2febd4de08f7650a30fd
Speykious <speykious@gmail.com>
2022-03-27 16:41:59 +0200

    No implicit usings

--- Examples/AutoGen/NativeFileDialogs.Net.Example.OpenDialog/Program.cs
+++ Examples/AutoGen/NativeFileDialogs.Net.Example.OpenDialog/Program.cs
@@ -2,6 +2,7 @@
 // This file is part of NativeFileDialogs.NET.
 // NativeFileDialogs.NET is licensed under the Zlib License. See LICENSE for details.
 
+using System;
 using NativeFileDialogs.AutoGen;
 
 unsafe
